9e5e613cda8165ac3f67f79f0ee59473ec3c3320
Eduar <eduarherrera0507@gmail.com>
2025-09-27 01:49:46 -0500

    Se hace cambio en front para que la info venga dinamica y no quemada desde la base de datos

--- demo/src/main/java/com/proyecto1/demo/Controllers/MainController.java
+++ demo/src/main/java/com/proyecto1/demo/Controllers/MainController.java
@@ -1,18 +1,32 @@
 package com.proyecto1.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.proyecto1.demo.Models.Entity.DAO.ProductoDAO;
 
 @Controller
 public class MainController {
 
+    private final ProductoDAO productoDAO;
+
+    public MainController(ProductoDAO productoDAO) {
+        this.productoDAO = productoDAO;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        loadProductos(model);
         return "index";
     }
     
     @GetMapping("/index")
-    public String indexPage() {
+    public String indexPage(Model model) {
+        loadProductos(model);
         return "index";
+    }
+
+    private void loadProductos(Model model) {
+        model.addAttribute("productos", productoDAO.findAll());
     }
 }
